6d1c83f0823d11eeaa100d8f60d393d1db6b0a17
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-27 12:26:26 -0400

    Function replacements now replace the parameters with the new function protorype, updated tests, output sommands being executed

--- test_app/fsw/src/the_app.c
+++ test_app/fsw/src/the_app.c
@@ -1,46 +1,46 @@
-TRUE -> true
-FALSE -> false
-boolean -> bool
-CFE_SB_CMD_HDR_SIZE -> sizeof(CFE_MSG_CommandHeader_t)
-CFE_SB_TLM_HDR_SIZE -> sizeof(CFE_MSG_TelemetryHeader_t)
-CFE_ES_APP_ERROR -> CFE_ES_RunStatus_APP_ERROR
-CFE_ES_APP_RUN -> CFE_ES_RunStatus_APP_RUN
-CFE_EVS_ERROR -> CFE_EVS_EventType_ERROR
-CFE_EVS_INFORMATION -> CFE_EVS_EventType_INFORMATION
-CFE_EVS_DEBUG -> CFE_EVS_EventType_DEBUG
-CFE_SB_MsgPtr_t -> CFE_MSG_Message_t *
-CFE_SB_Msg_t -> CFE_MSG_Message_t
-CFE_ES_RegisterChildTask(); -> remove
-CFE_ES_RegisterApp(); -> remove
-CFE_SB_GetTotalMsgLength(old parameters) -> CFE_MSG_GetSize(new parameters)
-CFE_SB_SetTotalMsgLength(old parameters) -> CFE_MSG_SetSize(new parameters)
-CFE_SB_GetMsgId(old parameters) -> CFE_MSG_GetMsgId(new parameters)
-CFE_SB_GetCmdCode(old parameters) -> CFE_MSG_GetFcnCode(new parameters)
-CFE_SB_SetCmdCode(old parameters) -> CFE_MSG_SetFcnCode(new parameters)
-CFE_SB_InitMsg(old parameters) -> CFE_MSG_Init(new parameters)
-CFE_SB_SendMsg(old parameters) -> CFE_SB_TransmitMsg(new parameters)
+TRUE -> true 
+FALSE -> false 
+boolean -> bool 
+CFE_SB_CMD_HDR_SIZE -> sizeof(CFE_MSG_CommandHeader_t) 
+CFE_SB_TLM_HDR_SIZE -> sizeof(CFE_MSG_TelemetryHeader_t) 
+CFE_ES_APP_ERROR -> CFE_ES_RunStatus_APP_ERROR 
+CFE_ES_APP_RUN -> CFE_ES_RunStatus_APP_RUN 
+CFE_EVS_ERROR -> CFE_EVS_EventType_ERROR 
+CFE_EVS_INFORMATION -> CFE_EVS_EventType_INFORMATION 
+CFE_EVS_DEBUG -> CFE_EVS_EventType_DEBUG 
+CFE_SB_MsgPtr_t -> CFE_MSG_Message_t * 
+CFE_SB_Msg_t -> CFE_MSG_Message_t 
+CFE_ES_RegisterChildTask(); -> remove 
+CFE_ES_RegisterApp(); -> remove 
+CFE_SB_GetTotalMsgLength(parameter); -> CFE_MSG_GetSize(CFE_MSG_Message_t *MsgPtr, CFE_MSG_Size_t *Size);                   
+CFE_SB_SetTotalMsgLength(parameter, parameter); -> CFE_MSG_SetSize(CFE_MSG_Message_t *MsgPtr, CFE_MSG_Size_t Size); 
+CFE_SB_GetMsgId(parameter); -> CFE_MSG_GetMsgId(CFE_MSG_Message_t *MsgPtr, CFE_SB_MsgId_t *MsgId); 
+CFE_SB_GetCmdCode(parameter); -> CFE_MSG_GetFcnCode(CFE_MSG_Message_t *MsgPtr, CFE_MSG_FcnCode_t *FcnCode); 
+CFE_SB_SetCmdCode(parameter, parameter); -> CFE_MSG_SetFcnCode(CFE_MSG_Message_t *MsgPtr, CFE_MSG_FcnCode_t FcnCode); 
+CFE_SB_InitMsg(parameter, parameter, parameter, parameter); -> CFE_MSG_Init(CFE_MSG_Message_t *MsgPtr, CFE_SB_MsgId_t MsgId, CFE_MSG_Size_t Size); 
+CFE_SB_SendMsg(parameter); -> CFE_SB_TransmitMsg(CFE_MSG_Message_t *MsgPtr, bool IncrementSequenceCount); 
 
 
 
 //testing duplicates
-TRUE -> true
-FALSE -> false
-boolean -> bool
-CFE_SB_CMD_HDR_SIZE -> sizeof(CFE_MSG_CommandHeader_t)
-CFE_SB_TLM_HDR_SIZE -> sizeof(CFE_MSG_TelemetryHeader_t)
-CFE_ES_APP_ERROR -> CFE_ES_RunStatus_APP_ERROR
-CFE_ES_APP_RUN -> CFE_ES_RunStatus_APP_RUN
-CFE_EVS_ERROR -> CFE_EVS_EventType_ERROR
-CFE_EVS_INFORMATION -> CFE_EVS_EventType_INFORMATION
-CFE_EVS_DEBUG -> CFE_EVS_EventType_DEBUG
-CFE_SB_MsgPtr_t -> CFE_MSG_Message_t *
-CFE_SB_Msg_t -> CFE_MSG_Message_t
-CFE_ES_RegisterChildTask(); -> remove
-CFE_ES_RegisterApp(); -> remove
-CFE_SB_GetTotalMsgLength(old parameters) -> CFE_MSG_GetSize(new parameters)
-CFE_SB_SetTotalMsgLength(old parameters) -> CFE_MSG_SetSize(new parameters)
-CFE_SB_GetMsgId(old parameters) -> CFE_MSG_GetMsgId(new parameters)
-CFE_SB_GetCmdCode(old parameters) -> CFE_MSG_GetFcnCode(new parameters)
-CFE_SB_SetCmdCode(old parameters) -> CFE_MSG_SetFcnCode(new parameters)
-CFE_SB_InitMsg(old parameters) -> CFE_MSG_Init(new parameters)
-CFE_SB_SendMsg(old parameters) -> CFE_SB_TransmitMsg(new parameters)
+TRUE -> true 
+FALSE -> false 
+boolean -> bool 
+CFE_SB_CMD_HDR_SIZE -> sizeof(CFE_MSG_CommandHeader_t) 
+CFE_SB_TLM_HDR_SIZE -> sizeof(CFE_MSG_TelemetryHeader_t) 
+CFE_ES_APP_ERROR -> CFE_ES_RunStatus_APP_ERROR 
+CFE_ES_APP_RUN -> CFE_ES_RunStatus_APP_RUN 
+CFE_EVS_ERROR -> CFE_EVS_EventType_ERROR 
+CFE_EVS_INFORMATION -> CFE_EVS_EventType_INFORMATION 
+CFE_EVS_DEBUG -> CFE_EVS_EventType_DEBUG 
+CFE_SB_MsgPtr_t -> CFE_MSG_Message_t * 
+CFE_SB_Msg_t -> CFE_MSG_Message_t 
+CFE_ES_RegisterChildTask(); -> remove 
+CFE_ES_RegisterApp(); -> remove 
+CFE_SB_GetTotalMsgLength(parameter); -> CFE_MSG_GetSize(CFE_MSG_Message_t *MsgPtr, CFE_MSG_Size_t *Size);                   
+CFE_SB_SetTotalMsgLength(parameter, parameter); -> CFE_MSG_SetSize(CFE_MSG_Message_t *MsgPtr, CFE_MSG_Size_t Size); 
+CFE_SB_GetMsgId(parameter); -> CFE_MSG_GetMsgId(CFE_MSG_Message_t *MsgPtr, CFE_SB_MsgId_t *MsgId); 
+CFE_SB_GetCmdCode(parameter); -> CFE_MSG_GetFcnCode(CFE_MSG_Message_t *MsgPtr, CFE_MSG_FcnCode_t *FcnCode); 
+CFE_SB_SetCmdCode(parameter, parameter); -> CFE_MSG_SetFcnCode(CFE_MSG_Message_t *MsgPtr, CFE_MSG_FcnCode_t FcnCode); 
+CFE_SB_InitMsg(parameter, parameter, parameter, parameter); -> CFE_MSG_Init(CFE_MSG_Message_t *MsgPtr, CFE_SB_MsgId_t MsgId, CFE_MSG_Size_t Size); 
+CFE_SB_SendMsg(parameter); -> CFE_SB_TransmitMsg(CFE_MSG_Message_t *MsgPtr, bool IncrementSequenceCount); 
